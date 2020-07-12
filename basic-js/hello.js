ee5ecdbb52a6735a9f13d962be5a9a677d27034e
mimnets <mdmonirulislammonir@gmail.com>
2020-07-12 08:48:40 +0600

    learn js promise.touppercase(), promise.tolowercase() and promise.indexof

--- basic-js/hello.js
+++ basic-js/hello.js
@@ -5,3 +5,11 @@ console.log(typeof seenAfter);
 
 var name = "my address";
 console.log(name);
+
+
+var promise = "I promise I will work hard to become a progmmer";
+
+console.log(promise.toLowerCase());
+console.log(promise.toUpperCase());
+
+console.log(promise.indexOf('promise'));
