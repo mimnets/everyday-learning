bb5a44a3c53a1efb789cf320df633ddf019f5adf
mimnets <mdmonirulislammonir@gmail.com>
2020-07-12 09:09:57 +0600

    learn praseInt, praseFloat, prase.split(' '), put limit to show fraction result by total.toFixed(2)

--- basic-js/hello.js
+++ basic-js/hello.js
@@ -13,3 +13,31 @@ console.log(promise.toLowerCase());
 console.log(promise.toUpperCase());
 
 console.log(promise.indexOf('promise'));
+
+console.log(promise.split(' '));
+
+var number1 = 25;
+var number2 = '25.5';
+
+number2 = parseInt(number2);
+
+number1 = '' +number1;
+
+console.log(number1 + number2);
+
+var number3 = 0.1;
+var number4 = 0.5;
+
+var total = number3 + number4;
+total = total.toFixed(2);
+console.log(total);
+
+
+var price1 = 25;
+var price2 = 35;
+
+var total = price1 + price2;
+
+console.log(total);
+
+
