445f3609e00c490e0d9e79353597ed2c919c1354
mimnets <mdmonirulislammonir@gmail.com>
2020-07-12 10:14:57 +0600

    learn about date = new Date()

--- basic-js/conditionals.js
+++ basic-js/conditionals.js
@@ -5,3 +5,23 @@ if(biscuitPrice != 10){
 else{
     console.log("Khali cha is better");
 }
+
+
+
+var jobPaiso = false;
+var savingAmount = 500000;
+
+if(jobPaiso == true || savingAmount > 200000){
+    console.log("Aso tomar patri khuji");
+}
+else if(jobPaiso == true){
+    console.log("Koydin por patri dekha suru korbo");
+}
+else{
+    console.log("Tor kopale biya nai");
+}
+
+var date = new Date();
+date = new Date('1988-05-14')
+console.log(date);
+
