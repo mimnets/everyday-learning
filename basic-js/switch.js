b459d34f364e7e6f7976a61fcfade8353e8490c9
mimnets <mdmonirulislammonir@gmail.com>
2020-07-14 10:00:12 +0600

    learn swtich, case, break

---
+++ basic-js/switch.js
@@ -0,0 +1,12 @@
+var friends = 'MimCodder';
+switch (friends) {
+    case 'Monirul':
+         console.log('Hi MimCodder, I am your friend!');
+         break;
+    case 'Ripon' :
+    case 'MimCodder' :
+         console.log('Hi MimCodder, We are your friend, can you recognize us?');
+         break;
+        default:
+         console.log('Sorry, no friends found');
+}
