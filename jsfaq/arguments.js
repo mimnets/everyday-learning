687958a7f82845b2cc589db7c6092c64c57a5abf
mimnets <mdmonirulislammonir@gmail.com>
2020-07-21 10:36:38 +0600

    learn arguments - arguments is a array like, it works only inside function and we can pass all parameters with arguments

---
+++ jsfaq/arguments.js
@@ -0,0 +1,13 @@
+function addNumbers(num1, num2){
+    // console.log(arguments[3]); // Array like arguments
+    var sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        const num = arguments[i];
+        sum = sum + num;
+    }
+    return sum;
+}
+
+var result = addNumbers(3, 5, 7, 29, 6);
+
+console.log(result);
