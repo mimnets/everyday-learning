2efd033ecdfbffee1b6637be032ccb3e8ead0922
mimnets <mdmonirulislammonir@gmail.com>
2020-07-21 10:08:07 +0600

    callback function - pass function as a parameter

--- jsfaq/callBack.js
+++ jsfaq/callBack.js
@@ -17,7 +17,12 @@ function scrollFacebook(){
     console.log("Scroll facebook, but don't like post" );
 }
 
+function goToBed(){
+    console.log('Go to bed for sleeping');
+}
+
 explain_callback('Sogir Uddin',18, washHand);
 explain_callback('Jogir Uddin',12, takeShower);
 explain_callback('Mugir Uddin',22, scrollFacebook);
+explain_callback('Kabir Uddin',24, goToBed);
 
