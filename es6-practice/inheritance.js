8cfc26e7f6ae7d8e902a4ef953241e4e036349a8
mimnets <mdmonirulislammonir@gmail.com>
2020-08-08 00:10:27 +0600

    class inheritance

---
+++ es6-practice/inheritance.js
@@ -0,0 +1,13 @@
+class Child{
+    constructor(name){
+        this.name = name;
+    }
+}
+
+
+
+const baby = new Child("Arnold");
+const baby2 = new Child("Tom");
+
+console.log(baby);
+console.log(baby2);
