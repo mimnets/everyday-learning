edc595b55c0da47fb22786eaef28cb065ad40ba3
mimnets <mdmonirulislammonir@gmail.com>
2020-08-08 01:05:18 +0600

    truthy and falsy value

--- advanced-js/true-false.js
+++ advanced-js/true-false.js
@@ -2,6 +2,21 @@ const age = 4;
 
 const name = "Solaiman";
 
+// False value
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false 
+
+// Truthy value
+// '0' 
+//' ' 
+//[] 
+//{}
+//'false'
+
 if( age > 0 ){
     console.log("Condition is true");
 }else{
