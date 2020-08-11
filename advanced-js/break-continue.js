2778cef7f150ff78f546b125d29fac1168a0bb97
mimnets <mdmonirulislammonir@gmail.com>
2020-08-11 20:59:22 +0600

    windows commit

--- advanced-js/break-continue.js
+++ advanced-js/break-continue.js
@@ -1,7 +1,7 @@
 // const nums = [5,6,3,4,8,9,6,4];
 const nums = [5,6,-3,4,-8,9,-6,4];
 
-for(let i = 0; i < nums.length, i++){
+for(let i = 0; i < nums.length; i++){
     // if(nums[i] > 6){
     //     break;
     // }
