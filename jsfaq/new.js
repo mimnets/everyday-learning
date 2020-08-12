b58177cb678341e2ce9f02ca620d0970f56e655f
mimnets <mdmonirulislammonir@gmail.com>
2020-08-12 19:03:53 +0600

    new commit

---
+++ jsfaq/new.js
@@ -0,0 +1,23 @@
+class Person {
+    constructor(firstName, lastName, salary){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+}
+
+const heroPerson = new Person('Hero', 'Balam', 25000);
+console.log(heroPerson);
+
+const friendlyPerson = new Person('Hero', 'Kalam', 34000);
+console.log(friendlyPerson);
+
+// Object creation with function in old way
+
+function Person1(firstName, lastName, salary){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.salary = salary;
+}
+
+const oldPerson = 
