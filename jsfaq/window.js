ba567ef008c78ac2cf32feea99e805e2c8d12654
mimnets <mdmonirulislammonir@gmail.com>
2020-08-12 16:08:10 +0600

    global variable, window or without var

--- jsfaq/window.js
+++ jsfaq/window.js
@@ -1,10 +1,16 @@
 var name = "Monirul"
 
 function add (num1, num2){
+    // window.result = num1 + num2; // declared as a global variable
+    // result = num1 + num2; // without var it is also declared as a global variable
     var result = num1 + num2;
     console.log("result inside", result)
     console.log("name inside", name)
-    return result;
+    function double(num){
+        return num * 2;
+    }
+    var total = double(result);
+    return total;
 }
 
 // console.log("result outside", result)
