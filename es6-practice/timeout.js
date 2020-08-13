2ab509cf3064f23c89d681e6f3a1327d094b870e
mimnets <mdmonirulislammonir@gmail.com>
2020-08-14 00:20:30 +0600

    setTimeout and setInterval

---
+++ es6-practice/timeout.js
@@ -0,0 +1,17 @@
+function doSomething(){
+    console.log(33333);
+}
+console.log(1234);
+// setTimeout(doSomething, 5000);
+// setTimeout(function (){
+//     console.log('Lazy and waiting');
+// }, 4000);
+setInterval(function(){
+    console.log('doing it');
+}, 1000)
+console.log(54545);
+console.log(54545);
+console.log(54545);
+console.log(54545);
+console.log(54545);
+console.log(54545);
