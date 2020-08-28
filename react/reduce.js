c80bf28515ca9abbdf3f2d823e110a70e8f4cfa0
MiMNets <mdmonirulislammonir@gmail.com>
2020-08-28 21:59:21 +0600

    reduce explained in multiline statement

--- react/reduce.js
+++ react/reduce.js
@@ -2,4 +2,8 @@ const nums = [12, 25, 34, 65, 23];
 nums.map(num => num * 2);
 nums.filter(num => num > 20);
 nums.find(num => num > 23);
-nums.reduce( (sum, num) => sum + num , 0)
+const total = nums.reduce( (sum, num) => {
+    console.log(sum, num);
+    return sum + num
+} , 0);
+console.log(total);
