db1bcf40331808d69114d8cbabf4c5043014b1df
MiMNets <mdmonirulislammonir@gmail.com>
2020-08-28 22:08:04 +0600

    run reduce in a friends array with objects data

--- react/reduce.js
+++ react/reduce.js
@@ -1,9 +1,16 @@
-const nums = [12, 25, 34, 65, 23];
-nums.map(num => num * 2);
-nums.filter(num => num > 20);
-nums.find(num => num > 23);
-const total = nums.reduce( (sum, num) => {
-    console.log(sum, num);
-    return sum + num
+// const nums = [12, 25, 34, 65, 23];
+const friends = [
+    { name: 'Monir', money: 23},
+    { name: 'Lonir', money: 43},
+    { name: 'Gonir', money: 33},
+    { name: 'Tonir', money: 25},
+    { name: 'Bonir', money: 34}
+]
+// nums.map(num => num * 2);
+// nums.filter(num => num > 20);
+// nums.find(num => num > 23);
+const total = friends.reduce( (sum, friend) => {
+    console.log(sum, friend);
+    return sum + friend.money;
 } , 0);
 console.log(total);
